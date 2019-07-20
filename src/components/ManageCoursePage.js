ba2e6a31eecf654893303b46545a16689a7bafb7
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-07-20 12:34:50 +0400

    Add validation for Form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -4,6 +4,7 @@ import * as courseAPI from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -21,8 +22,20 @@ const ManageCoursePage = props => {
     setCourse(updatedCourse);
   }
 
+  function formIsValid() {
+    const _errors = {};
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     courseAPI.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course saved.");
@@ -33,6 +46,7 @@ const ManageCoursePage = props => {
     <>
       <h2>Manage Course</h2>
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
