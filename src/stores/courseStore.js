684deacaf345f7defdbe0b3ed61f58669e8a9252
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-07-20 19:50:18 +0400

    Add dispatcher registr and private strong

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,6 +1,9 @@
 import { EventEmitter } from "events";
+import Dispatcher from "../appDispatcher";
+import actionTypes from "../actions/actionTypes";
 
 const CHANGE_EVENT = "change";
+let _courses = [];
 class CourseStore extends EventEmitter {
   addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
@@ -13,6 +16,24 @@ class CourseStore extends EventEmitter {
   emit() {
     this.emit(CHANGE_EVENT);
   }
+
+  getCourses() {
+    return _courses;
+  }
+
+  getCourseBySlug(slug) {
+    _courses.find(course => course.slug === slug);
+  }
 }
 const store = new CourseStore();
+
+Dispatcher.register(action => {
+  switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    default:
+  }
+});
 export default store;
